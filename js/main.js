93308fcf64929d32402802dcb2e23253c0050397
Junani <paul911014@naver.com>
2018-05-02 15:06:19 +0900

    위치 수정

--- js/main.js
+++ js/main.js
@@ -12,12 +12,13 @@ setInterval(function(){
   window.addEventListener('load', function(){
     if(typeof web3 !== 'undefined'){
       window.web3 = new Web3(web3.currentProvider);
+      startApp();
     } else{
       console.log('No web3? You should consider trying MetaMask!');
       window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
     }
 
-    startApp();
+
   });
 
   function startApp(){
