127f4a7e40168659e3f5e52c5411eba8641a30cd
Junani <paul911014@naver.com>
2018-05-02 14:45:46 +0900

    set interval 수정
    버튼동작은 제거

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var savedAddress = "";
+var savedBalances = 0;
+
 setInterval(function(){
   var contractAddress = '0xdc7c743110Ec689d82f050302319AE27aF84f8fa';
   var abi = [{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"comment","type":"string"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":false,"name":"count","type":"uint256"}],"name":"Commented","type":"event"},{"constant":false,"inputs":[{"name":"comment","type":"string"}],"name":"makeComment","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"blockByCounter","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"content","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"numComments","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
@@ -18,26 +21,25 @@ setInterval(function(){
   });
 
   function startApp(){
+
     declaration = web3.eth.contract(abi).at(contractAddress);
 
-    web3.eth.getAccounts(function(e,r){
-      if(!e){
-        myAddr = r[0];
-        document.getElementById('accountAddr').innerHTML = myAddr;
+    web3.eth.getCoinbase(function(e,address){
 
-        web3.eth.getBalance(myAddr, function(e, balance){
+      web3.eth.getBalance(myAddr, function(e, balance){
+
+        if(((address != null) && (savedAddress != address)) || (savedBalances != balance)){
+          document.getElementById('accountAddr').innerHTML = address;
           document.getElementById('accountBal').innerHTML = Number(web3.fromWei(Number(balance), 'ether')).toFixed(2) + "ETH";
-        });
-      } else{
-        console.log(err);
-      }
-    });
 
+          savedAddress = address;
+          savedBalances = balance;
+        }
+
+      });
+
+    });
 
   }
 
 }, 1000);
-
-function transaction(){
-  alert("hello");
-}
