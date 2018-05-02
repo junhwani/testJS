313c97096add908e553d5170c51ca75cb5334ebd
Junani <paul911014@naver.com>
2018-05-02 18:00:46 +0900

    간단하게 alert로

--- js/button.js
+++ js/button.js
@@ -1,6 +1,7 @@
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName('close')[0];
+var btn_send = document.getElementById('sendBtn');
 
 btn.onclick = function(){
   modal.style.display = "block";
@@ -14,4 +15,11 @@ window.onclick = function(event){
   if(event.target == modal){
     modal.style.display = "none";
   }
+}
+
+btn_send.onclick = function(){
+  var address = document.getElementById('receive_address').value;
+  var eth = document.getElementById('ethAmount').value;
+
+  alert(address + ", " + eth);
 }
