ea9c445973aadec05c27788f9c990384597df415
Junani <paul911014@naver.com>
2018-05-02 15:45:05 +0900

    setInterval 놓칠수없다

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var savedAddress = "";
 var savedBalances = 0;
 
+setInterval(function(){
 var contractAddress = '0xdc7c743110Ec689d82f050302319AE27aF84f8fa';
 var abi = [{"anonymous":false,"inputs":[{"indexed":true,"name":"sender","type":"address"},{"indexed":false,"name":"comment","type":"string"},{"indexed":false,"name":"timestamp","type":"uint256"},{"indexed":false,"name":"count","type":"uint256"}],"name":"Commented","type":"event"},{"constant":false,"inputs":[{"name":"comment","type":"string"}],"name":"makeComment","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"blockByCounter","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"content","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"numComments","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
 var declaration;
-var myAddr;
 var startingBlock;
 var explorer = "https://etherscan.io";
 
@@ -30,8 +30,13 @@ function startApp(){
       document.getElementById("accountAddr").innerHTML = address;
       document.getElementById("accountBal").innerHTML = Number(web3.fromWei(Number(balance), 'ether')).toFixed(2)+" ETH";
 
+      savedAddress = address;
+      savedBalances = balance;
+
     });
 
   });
 
 }
+
+}, 1000);
